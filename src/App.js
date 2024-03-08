8bc08c998bdd711b76b138905b7d64146f080ee7
Shubhangi Naik <shubhanginaik@Shubhangis-MacBook-Air.local>
2024-03-08 19:10:06 +0200

    added import note

--- src/App.js
+++ src/App.js
@@ -2,3 +2,7 @@ function App() {
   return <h1>Hi there!</h1>;
 }
 export default App;
+
+//named export
+
+export const message = "Bye there!";
--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+//importing a default export, we can rename it from App->myApp
 import App from "./App";
+
+// importing a non-default object/export
+//named exports cannot be renamed
+import { message } from "./App";
 
 const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el);
+<h2>{message}</h2>;
 
 root.render(<App />);
