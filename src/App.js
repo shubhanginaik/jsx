56296c54922c37e06f39c1ab692ed4bd13e7c840
Shubhangi Naik <shubhanginaik@Shubhangis-MacBook-Air.local>
2024-03-09 22:35:33 +0200

    added bulma css

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import "bulma/css/bulma.css";
 import ProfileCard from "./ProfileCard";
 import AlexaImage from "./images/alexa.png";
 import CortanaImage from "./images/cortana.png";
@@ -7,13 +8,29 @@ function App() {
   return (
     <div>
       <div>Personal Digital Asistannts</div>
-      <ProfileCard title="Alexa" handle="@alexa99" imageSrc={AlexaImage} />
-      <ProfileCard
-        title="Cortana"
-        handle="@cortana32"
-        imageSrc={CortanaImage}
-      />
-      <ProfileCard title="Siri" handle="@siri01" imageSrc={SiriImage} />
+      <div className="container">
+        <div className="section">
+          <div className="columns">
+            <div className="column is-4">
+              <ProfileCard
+                title="Alexa"
+                handle="@alexa99"
+                imageSrc={AlexaImage}
+              />
+            </div>
+            <div className="column is-4">
+              <ProfileCard
+                title="Cortana"
+                handle="@cortana32"
+                imageSrc={CortanaImage}
+              />
+            </div>
+            <div className="column is-4">
+              <ProfileCard title="Siri" handle="@siri01" imageSrc={SiriImage} />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,10 +1,18 @@
 function ProfileCard({ title, handle, imageSrc }) {
   //const { title, handle } = props;
   return (
-    <div>
-      <img src={imageSrc} alt="apo img" />
-      <div>{title}</div>
-      <div>{handle}</div>
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-4by3">
+          <img src={imageSrc} alt="apo img" className="card-image" />
+        </figure>
+      </div>
+      <div className="card-content">
+        <div className="media-content">
+          <p className="title is-4">{title}</p>
+          <p className="title is-6">{handle}</p>
+        </div>
+      </div>
     </div>
   );
 }
